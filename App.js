9a2b061ebfd760fe9febf5443b4b8e90e41078e7
SaralaSharan <saralask2@gmail.com>
2024-07-15 11:36:50 -0400

    starter

---
+++ App.js
@@ -0,0 +1,12 @@
+import { s } from "./App.style";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Home } from "./pages/Home/Home";
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Home />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
